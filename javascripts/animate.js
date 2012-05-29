5045a2e68afc47cd3d56d989fb516c1af940f3fb
i.taroy@gmail.com <ingrid_taroy@Ingrid-Taroys-MacBook-Pro.local>
2012-05-29 10:47:06 -0700

    opp

--- javascripts/animate.js
+++ javascripts/animate.js
@@ -12,13 +12,12 @@ if (document.images) {     // Preloaded images
     demo1.src = "Images/home/purpleporn3.jpg";
     
     demo2 = new Image();
-    demo2.src = "Images/home/purple2.jpg";
+    demo2.src = "Images/home/purple4.jpg";
     
     demo3 = new Image();
     demo3.src = "Images/home/purple1.jpg";
     
-    demo4 = new Image();
-    demo4.src = "Images/home/purple4.jpg";
+   
 }
 
 function timeimgs(numb) {  // Reusable timer
@@ -28,8 +27,8 @@ function timeimgs(numb) {  // Reusable timer
 function imgturn(numb) {   // Reusable image turner
     if (document.images) {
 
-        if (numb == "4") {         // This will loop the image
-            document["demo"].src = eval("demo4.src");
+        if (numb == "3") {         // This will loop the image
+            document["demo"].src = eval("demo3.src");
             timeimgs('1');
         }
 
---
+++ javascripts/lib/change.js
@@ -0,0 +1,11 @@
+$(function() {
+             $('#change-big .dumbItem').dumbCrossFade({
+                 'slideType':'fade','transitionTime':2000,'showTime':7000
+             });
+             $('#change-thin .dumbItem').dumbCrossFade({
+                  'slideType':'fade','transitionTime':2000,'showTime':4000
+             });
+             $('#change-small .dumbItem').dumbCrossFade({
+                 'slideType':'fade','transitionTime':2000,'showTime':11000
+             });
+            });
